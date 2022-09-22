1842a336b563ac47bf76ec32eca3cb2596a613fc
sshubhamagg <shubhamsgl@invbrain.in>
2022-09-22 15:44:03 +0530

    Added methods to get supported tokens

---
+++ constants/index.js
@@ -0,0 +1,9 @@
+module.exports ={
+    supportedChains : ['ethereum', 'polygon', 'bsc' ],
+    oneInchBaseURL : "https://safle.api.enterprise.1inch.exchange/v4.0",
+    pancakeBaseURL: "https://tokens.pancakeswap.finance/pancakeswap-extended.json",
+    uniswapBaseURL: "https://tokens.uniswap.org/",
+    errorMessages:{
+        INVALID_CHAIN: "Invalid chain selected"
+    }
+}
