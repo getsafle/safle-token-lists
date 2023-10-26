3585a4f4653d88697eec5c4a0f85cae37d46373d
Shubham Singhal <shubham@getsafle.com>
2023-10-26 11:59:49 +0530

    Updated 1Inch API URL and request handling

--- constants/index.js
+++ constants/index.js
@@ -1,9 +1,10 @@
 module.exports ={
     supportedChains : ['ethereum', 'polygon', 'bsc' ],
-    oneInchBaseURL : "https://api-safle.1inch.io/v5.0",
+    oneInchBaseURL : "https://api.1inch.dev/swap/v5.2",
     pancakeBaseURL: "https://tokens.pancakeswap.finance/pancakeswap-extended.json",
     uniswapBaseURL: "https://tokens.uniswap.org/",
     errorMessages:{
         INVALID_CHAIN: "Invalid chain selected"
-    }
+    },
+    oneinchAuthToken: 'I00gf2qW3ebQSNZrcLrfmZelxmLxqdyL'
 }
